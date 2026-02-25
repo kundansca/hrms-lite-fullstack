20774843bfd53eed287c600c953be5371b27b783
kundansca <kksdarmiya@gmail.com>
2026-02-25 16:02:11 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = require("./config/database");
+const employeeRoutes = require("./routes/employeeRoutes");
+const attendanceRoutes = require("./routes/attendanceRoutes");
+const {
+  notFound,
+  globalErrorHandler,
+} = require("./middlewares/globalErrorHandler");
 
 const app = express();
 //! Establish connection to mongodb
@@ -11,6 +17,10 @@ connectDB();
 app.use(express.json());
 app.use("/api/employees", employeeRoutes);
 app.use("/api/attendance", attendanceRoutes);
+//? Not found error handler
+app.use(notFound);
+//? Setup the global error handler
+app.use(globalErrorHandler);
 const PORT = process.env.PORT || 9080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
